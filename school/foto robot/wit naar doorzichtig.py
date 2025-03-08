82aea3b5c349f59c132a8c0e9b849014605bcb83
Debakker Stan <stan.debakker@leerling.pti.be>
2025-03-08 19:25:54 +0100

    bug uit code gehaald waardoor het niet update

--- school/foto robot/wit naar doorzichtig.py
+++ school/foto robot/wit naar doorzichtig.py
@@ -3,11 +3,15 @@ tijd=time()
 from PIL import Image
 image = Image.open("image1.jpeg")
 pixels=image.load()
+foto_nieuw =Image.new("RGBA", (500, 499), (0,0,0,0))
+pixels_nieuw = foto_nieuw.load()
 for x in range(500):
     for y in  range(499):
         if pixels[x,y]==(255,255,255):
-            pixels[x,y]=(255,255,255,0)
+            pixels_nieuw[x,y]=(0,255,255,0)
+        else:
+            pixels_nieuw[x,y]=pixels[x,y]
 
-image.save("robot.png")
+foto_nieuw.save("robot.png")
 print(time()-tijd)
-image.show()
+foto_nieuw.show()
